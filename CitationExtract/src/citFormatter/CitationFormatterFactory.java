e4181d2a87fb1db5a57d24e5ddf391e7caa05145
drygaay <drygaay@NCBIPC1140.be-md.ncbi.nlm.nih.gov>
2014-01-09 11:31:14 -0500

    ciatition factory for rules applied to  ref and abstract text

---
+++ CitationExtract/src/citFormatter/CitationFormatterFactory.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citFormatter;
+
+import citExtract.Citation;
+import citExtract.CitationFormatted;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author drygaay
+ */
+public class CitationFormatterFactory {
+
+    public CitationFormatterFactory(ReferenceFormatter refFormatter, AbstractFormatter absFormatter) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     *
+     * @param citations list of parsed citations
+     * @return list of citations that are formatted according to rules
+     */
+    public List<CitationFormatted> format(List<Citation> citations) {
+        return null;
+    }
+}
