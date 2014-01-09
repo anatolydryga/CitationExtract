556baccff5a6418c956fd74fb7946dab7a6df9bd
drygaay <drygaay@NCBIPC1140.be-md.ncbi.nlm.nih.gov>
2014-01-09 11:30:33 -0500

    ciatition with rules applied for ref and abstract formated

---
+++ CitationExtract/src/citExtract/CitationFormatted.java
@@ -0,0 +1,34 @@
+package citExtract;
+
+import java.util.List;
+import substitution.*;
+
+/**
+ * parsed and formatted Citation consists of reference, article abstract,
+ * substitutions and status.
+ *
+ */
+public class CitationFormatted {
+
+    private final int PMID;
+    private final String ref;
+    private final List<Substitution> substitutions;
+
+    CitationFormatted(int PMID, String ref, List<Substitution> substitutions) {
+        this.PMID = PMID;
+        this.ref = ref;
+        this.substitutions = substitutions;
+    }
+
+    public int getPmid() {
+        return PMID;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public List<Substitution> getSubstitutions() {
+        return substitutions;
+    }
+}
