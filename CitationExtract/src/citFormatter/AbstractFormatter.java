7b56e3ffb9d6231d0c270fa9bb83e6ff528f4ff1
drygaay <drygaay@NCBIPC1140.be-md.ncbi.nlm.nih.gov>
2014-01-10 15:35:21 -0500

    format abstract accoring to set of rules and remove labels in structured abstract

--- CitationExtract/src/citFormatter/AbstractFormatter.java
+++ CitationExtract/src/citFormatter/AbstractFormatter.java
@@ -4,10 +4,55 @@
  */
 package citFormatter;
 
+import java.util.ArrayList;
+import java.util.List;
+import substitution.Substitution;
+import citExtract.Abstract;
+import substitution.GreekRule;
+import substitution.MicroRule;
+import substitution.SubstitutionRule;
+import substitution.TextRule;
+import substitution.TextSubstitution;
+
 /**
  *
  * @author drygaay
  */
 public class AbstractFormatter {
-    
+
+    static final ArrayList<SubstitutionRule> rules = new ArrayList<>();
+
+    static {
+        final SubstitutionRule hRule     = new TextRule("h", "hr");
+        rules.add(hRule);
+        final SubstitutionRule mlRule    = new TextRule("Ml", "mL");
+        rules.add(mlRule);
+        final SubstitutionRule mkgRule   = new TextRule("Microg", "ug");
+        rules.add(mkgRule);
+        final SubstitutionRule mklRule   = new TextRule("Microl", "uL");
+        rules.add(mklRule);
+        final SubstitutionRule microRule   = new MicroRule();
+        rules.add(microRule);
+        final SubstitutionRule greekRule = new GreekRule();
+        rules.add(greekRule);           
+    }
+
+    public List<Substitution> format(Abstract abs) {
+        List<Substitution> subs = new ArrayList<>();
+        List<String> sectionText  = abs.getSectionsText();
+        List<String> sectionLabel = abs.getSectionsLabel();
+        for (int i = 0;  i < sectionText.size();  i++) {
+            // we dont need labels at present
+            if ( ! sectionLabel.get(i).equals("")) {
+                String spaceOpt = (i == 0) ? "" : " "; 
+                subs.add(new Substitution(sectionLabel.get(i), spaceOpt));
+            }
+            TextSubstitution formatter = 
+                    new TextSubstitution(sectionText.get(i), rules);
+            for (Substitution s : formatter) {
+                subs.add(s);
+            }
+        }
+        return subs;
+    }
 }
