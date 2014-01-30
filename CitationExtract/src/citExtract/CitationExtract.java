4c0552b1de2a0c6e812f137decb063eb87f4663c
drygaay <drygaay@NCBIPC1140.be-md.ncbi.nlm.nih.gov>
2014-01-30 18:28:13 -0500

    write to file method

--- CitationExtract/src/citExtract/CitationExtract.java
+++ CitationExtract/src/citExtract/CitationExtract.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 import org.w3c.dom.Document;
 import citFormatter.*;
 import citPrinter.CitationPrinter;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**                                                                              
     main class for program that extract citation from NCBI                       
@@ -18,7 +23,9 @@ public class CitationExtract {
         try {                                                                    
             idsVal = new IdsValidator(args);                        
         } catch (IllegalArgumentException e) {                                   
-            System.out.println("Cannot process CMD line args: " + e);            
+            System.out.println();
+            System.out.println("FATAL ERROR:");
+            System.out.println("Cannot process CMD line args: " + e.getMessage());            
             System.out.println("Citation Extract failed!");                       
             System.exit(1);                                                      
         }                                                                        
@@ -27,9 +34,11 @@ public class CitationExtract {
         try {
             downloader = new PubMedDownloader(idsVal.getIds());           
         } catch (IllegalArgumentException e) {                                          
+            System.out.println();
             System.out.println("FATAL ERROR:");
-            System.out.println(e);
+            System.out.println(e.getMessage());
             System.out.println("Cannot Download PMIDs from NCBI!");                 
+            System.out.println("Check that PMIDs exist in PubMed.");                 
             System.exit(1);                                                             
         }                                                                        
                                                                                  
@@ -56,6 +65,20 @@ public class CitationExtract {
     }                                                                            
 
     private static void writeToFile(String htmlFile, String html) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(htmlFile, "UTF-8");
+            writer.println(html);
+            writer.close(); 
+        } catch (FileNotFoundException ex) {
+            System.out.println("");
+            System.out.println("ERROR: CITATIONS ARE NOT SAVED!");
+            System.out.println("Please close file \"" + htmlFile + "\"");
+            System.out.println("(or delete/rename it) before running the program.");
+            System.out.println("And rerun the program.");
+            System.out.println("");
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(CitationExtract.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }                       
