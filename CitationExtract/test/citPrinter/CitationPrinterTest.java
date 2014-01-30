69b5c90c4da7a14d9ac15752ed376e2e7194b153
drygaay <drygaay@NCBIPC1140.be-md.ncbi.nlm.nih.gov>
2014-01-30 18:47:56 -0500

    tests of citation to html

--- CitationExtract/test/citPrinter/CitationPrinterTest.java
+++ CitationExtract/test/citPrinter/CitationPrinterTest.java
@@ -1,58 +1,110 @@
 package citPrinter;
 
+import citExtract.Citation;
+import citExtract.CitationFactory;
 import citExtract.CitationFormatted;
-import org.junit.After;
-import org.junit.Before;
+import citExtract.FileReader;
+import citFormatter.AbstractFormatter;
+import citFormatter.CitationFormatterFactory;
+import citFormatter.ReferenceFormatter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.w3c.dom.Document;
 
 /**
  *
  * @author drygaay
  */
 public class CitationPrinterTest {
+
+    CitationPrinter printer;
     
     public CitationPrinterTest() {
+        Document doc1 = null; // structured abstract, no subsititutions
+        Document doc2 = null; // abstract with \mu
+        Document doc3 = null; // 2 abstracts: nonthing in 1st, h, greek and approx in 2nd 
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            doc1 = db.parse("./test/citExtract/16545112.xml");
+            doc2 = db.parse("./test/citExtract/24015326.xml");
+            doc3 = db.parse("./test/citExtract/21887247_124.xml");
+        } catch (Exception e) {
+            assertTrue(false);
+        }
+        List<Citation> citations1 = CitationFactory.createCitations(doc1);
+        List<Citation> citations2 = CitationFactory.createCitations(doc2);
+        List<Citation> citations3 = CitationFactory.createCitations(doc3);
+
+        List<Citation> citations =  new ArrayList<>();
+
+        citations.addAll(citations1);
+        citations.addAll(citations2);
+        citations.addAll(citations3);
+
+        ReferenceFormatter refFormatter = new ReferenceFormatter();
+        AbstractFormatter absFormatter = new AbstractFormatter(); 
+        CitationFormatterFactory instance = 
+                new CitationFormatterFactory(refFormatter, absFormatter);
+        List<CitationFormatted>  result = instance.format(citations);
+        printer = new CitationPrinter(result);
     }
 
     @Test
     public void testPrint() {
-        System.out.println("print");
-        CitationPrinter instance = null;
-        String expResult = "";
-        String result = instance.print();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        String result = printer.print();
+        assertTrue(result.contains("<html>"));
+        assertTrue(result.contains("PMID"));
+        assertTrue(result.contains("16545112"));
+        assertTrue(result.contains("24015326"));
+        assertTrue(result.contains("21887247"));
+        assertTrue(result.contains("124"));
+        assertTrue(result.contains("http://www.ncbi.nlm.nih.gov/pubmed/?term="));
+        assertTrue(result.contains("</html>"));
     }
 
     @Test
     public void testPrintCitation() {
-        System.out.println("printCitation");
-        CitationFormatted citation = null;
-        CitationPrinter instance = null;
-        String expResult = "";
-        String result = instance.printCitation(citation);
+        String expResult = "<br>PMID = <a href = " +
+                "\"http://www.ncbi.nlm.nih.gov/pubmed/?term=16545112\">16545112</a>" +
+                "<br><br>Snyder KA et al;" +
+                " BMC Bioinformatics 7: 152 (2006)<br><br>";
+        String abstractFormatted = ""; 
+        try { 
+            abstractFormatted = FileReader.readFile(
+             "./test/citExtract/16545112_abstractFormatted.dat");
+        } catch (IOException ex) {
+            fail("cannot open 16545112_abstractFormatted.dat");
+        }
+        expResult += abstractFormatted;
+        expResult += "<br>";
+        String result = printer.printCitation(0);
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testPrintHeader() {
-        System.out.println("printHeader");
-        CitationPrinter instance = null;
-        String expResult = "";
-        String result = instance.printHeader();
+        String expResult = "<!DOCTYPE html>\n" +
+                           "<meta charset=\"UTF-8\">\n" + 
+                           "<html>\n" +
+                           "    <head>\n" +
+                           "        <title>Citation Formatter</title>\n" +
+                           "    </head>\n" +
+                           "    <body>\n";
+        String result = printer.printHeader();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testPrintTail() {
-        System.out.println("printTail");
-        CitationPrinter instance = null;
-        String expResult = "";
-        String result = instance.printTail();
+        String expResult = "    </body>\n" +
+                           "</html>\n";
+        String result = printer.printTail();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 }
