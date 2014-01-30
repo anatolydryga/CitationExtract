508aa535763b6ed6e5d8cbc2cb17703ce37cf694
drygaay <drygaay@NCBIPC1140.be-md.ncbi.nlm.nih.gov>
2014-01-30 18:44:49 -0500

    new rules for author list; if there is collective author it is always listed last

--- CitationExtract/src/citFormatter/ReferenceFormatter.java
+++ CitationExtract/src/citFormatter/ReferenceFormatter.java
@@ -17,30 +17,68 @@ public class ReferenceFormatter {
         List<String> authors = ref.getAuthorLastNames();
         List<String> initials = ref.getAuthorInitials();
 
-        for (int i = 0; i < authors.size(); i++) {
-            formatted.append(authors.get(i));
-            formatted.append(" ");
-            formatted.append(initials.get(i));
-            if (i != authors.size() - 1) {
-                formatted.append(", ");
-            }
-        }
-        formatted.append(". ");
+        formatted.append(getAuthors(authors, initials));
+        formatted.append(getCollectiveAuthor(authors, ref));
+        formatted.append("; ");
+
         formatted.append(ref.getJournal());
-        formatted.append(", ");
-        if (!ref.getVolume().equals("")) {
+        formatted.append(" ");
+        if ( ! ref.getVolume().equals("")) {
             formatted.append(ref.getVolume());
-            formatted.append(", ");
         }
-        if (!ref.getIssue().equals("")) {
+        if ( ! ref.getIssue().equals("")) {
+            formatted.append("(");
             formatted.append(ref.getIssue());
-            formatted.append(", ");
+            formatted.append(")");
         }
-        formatted.append(ref.getYear());
-        if (!ref.getPages().equals("")) {
-            formatted.append("; ");
+        formatted.append(":");
+        if ( ! ref.getPages().equals("")) {
+            formatted.append(" ");
             formatted.append(ref.getPages());
+            formatted.append(" ");
+        }
+        formatted.append("(");
+        formatted.append(ref.getYear());
+        formatted.append(")");
+        return formatted.toString();
+    }
+
+    private String getAuthors(List<String> authors, List<String> initials) {
+        StringBuilder formatted = new StringBuilder();
+
+        if (authors.size() == 0) {
+            return "";
+        }
+
+        assert(authors.size() > 0);
+        formatted.append(authors.get(0));
+        formatted.append(" ");
+        formatted.append(initials.get(0));
+
+        if (authors.size() == 2) {
+            formatted.append(", ");
+            formatted.append(authors.get(1));
+            formatted.append(" ");
+            formatted.append(initials.get(1));
+        }
+        if (authors.size() >= 3) {
+            formatted.append(" et al");
         }
         return formatted.toString();
+    }
+
+    private String getCollectiveAuthor(List<String> authors, Reference ref) {
+        String result = "";
+        if ( ref.getCollectiveName().equals("")) {
+            return "";
+        }
+
+        if (authors.size() == 0) {
+            result =  ref.getCollectiveName();
+        }
+        if (authors.size() == 1) {
+            result =  ", " + ref.getCollectiveName();
+        }
+        return result;
     }
 }
